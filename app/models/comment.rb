91d02998d63639781c0c062e741ac4f37a9f2044
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-28 01:13:58 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  # following is probably not needed
-  # def username
-  #   user ? self.user.username : nil
-  # end
+  # following is jsut to avoid view going 2 levels deep...
+  def username
+     user ? self.user.username : nil
+  end
 end
